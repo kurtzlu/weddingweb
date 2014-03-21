d3aad5ea806bd6994449053cf48af4ae16912af5
P05 <chris@sanyimp.com>
2014-03-21 08:56:15 +0800

    add rail on ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weddingweb::Application.config.secret_key_base = '8adfff2d2da4884d8fdc30a3e1747c921835858e91f606fd961560363ee9db94f47d07d22d76c2cd3df6dd2b41f410536613947e8d9ef2927860e2a7e3d9c5ad'
